2f851b86a8fb790f00e54008f2d313abe658cf3b
MariaAcevedo11 <macevedos2@eafit.edu.co>
2026-02-17 17:45:13 -0500

    4th Tutorial Completed

--- frontend/src/main.ts
+++ frontend/src/main.ts
@@ -1,14 +1,13 @@
 import './assets/css/input.css';
 
-import { createApp } from 'vue';
-import { createPinia } from 'pinia';
-
 import App from './App.vue';
+import { createApp } from 'vue';
+import PiniaConfig from './PiniaConfig.js';
 import router from './router';
 
 const app = createApp(App);
 
-app.use(createPinia());
+app.use(PiniaConfig.init());
 app.use(router);
 
 app.mount('#app');
